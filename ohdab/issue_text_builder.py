53e625b7bef9cec1a5dc591b7cb1904d133b0f1b
Deniz <denizjaeger@gmx.de>
2026-05-22 11:25:06 +0200

    chore: removed dev/test artifacts

--- ohdab/issue_text_builder.py
+++ ohdab/issue_text_builder.py
@@ -6,11 +6,11 @@ RESOURCES_DIR.mkdir(parents=True, exist_ok=True)
 def write_missing_parents_issue_text(classes_without_real_parent: list):
     output_file = "missing_parents_issue_text.md"
     with open(RESOURCES_DIR / output_file, "w", encoding="utf-8") as f:
-        f.write("# Classes without real parent\n\n")
+        f.write("## Classes without real parent\n\n")
         f.write("The OhdAB ontology contains the following classes, which do not have a parent class except themselves and therefore do not get sorted into the hierarchy:\n\n")
 
         for uri in classes_without_real_parent:
             f.write(f"- `{uri}`\n")
 
         f.write("\n\n")
-        f.write("cc @1, @2")
+        f.write("cc @OlafSimons, @FactGrid")
