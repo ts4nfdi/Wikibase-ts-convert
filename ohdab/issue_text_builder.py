22a78ce4c5665f9997e7e134a92646be7c993826
Deniz <denizjaeger@gmx.de>
2026-05-22 10:00:32 +0200

    feat: issue text file gets generated

---
+++ ohdab/issue_text_builder.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+RESOURCES_DIR = Path(__file__).resolve().parent / "resources"
+RESOURCES_DIR.mkdir(parents=True, exist_ok=True)
+
+def write_missing_hierarchy_issue_text(classes_without_real_parent: list):
+    output_file = "iris_missing_hierarchy_issue_text.txt"
+    with open(RESOURCES_DIR / output_file, "w", encoding="utf-8") as f:
+        f.write("# Classes without real parent\n\n")
+        f.write("The OhdAB ontology contains the following classes, which do not have a parent class except themselves and therefore do not get sorted into the hierarchy:\n\n")
+
+        for uri in classes_without_real_parent:
+            f.write(f"- `{uri}`\n")
+
+        f.write("\n\n")
+        f.write("cc @1, @2")
